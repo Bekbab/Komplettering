2a90f244c07ecd616cb39a7ac00ad62c6cd7046d
Bekbab <72729623+Bekbab@users.noreply.github.com>
2021-06-03 15:33:33 +0200

    save

--- Komplettering/Program.cs
+++ Komplettering/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
 
 namespace Komplettering
 {
+
     class Program
     {
+
+        static void ReturnAge(int age)
+        {
+            if (age > 0 && age < 120)
+            {
+                Console.WriteLine("Wow du är hela " + age + " år gammal? Vad stor du har blivit!");
+                ageGotten = true; //en korrekt ålder har skrivits in, programmet slutar loopas. 
+            }
+            else if (age > 120)
+            {
+                Console.WriteLine("Wow hela " + age + " år? Du är för gammal.");
+            }
+            else if (age < 0)
+            {
+                Console.WriteLine(age + " år? Du är int ens född än!");
+            }
+            else
+            {
+                Console.WriteLine("Det där är ingen giltig ålder");
+            }
+        }
+
+        public static bool ageGotten = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Spelets loop baseras på att den kommer att fortsätta fråga efter en ålder så länge den inte har blivit mellan 1 och 120
+            while (ageGotten == false)
+            {
+                Console.WriteLine("Hur gammal är du?");
+                string inputAge = Console.ReadLine();
+                int result;
+                bool isNumber = int.TryParse(inputAge, out result); //result blir 0 om inputAge innhåller bokstäver
+                ReturnAge(result);
+            }
+            Console.ReadLine();
         }
     }
 }
